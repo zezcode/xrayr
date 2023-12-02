e7994e3dc065c9a7374fbbb41995210315532e8c
OverKill DEV <140531897+overkillzero@users.noreply.github.com>
2023-12-02 14:40:49 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,213 @@
+#!/bin/bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+cur_dir=$(pwd)
+
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${red}Lỗi：${plain} Vui lòng chạy với quyền root (gõ lệnh sudo su để dùng quyền root)！\n" && exit 1
+
+# check os
+if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+else
+    echo -e "${red}Không định dạng được hệ điều hành, hãy thử lại！${plain}\n" && exit 1
+fi
+
+arch=$(arch)
+
+if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
+    arch="64"
+elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
+    arch="arm64-v8a"
+elif [[ $arch == "s390x" ]]; then
+    arch="s390x"
+else
+    arch="64"
+    echo -e "${red}Không xác định được phiên bản: ${arch}${plain}"
+fi
+
+echo "Cấu trúc CPU: ${arch}"
+
+if [ "$(getconf WORD_BIT)" != '32' ] && [ "$(getconf LONG_BIT)" != '64' ] ; then
+    echo "Phần mềm không hỗ trợ hệ thống 32bit, hãy thử với hệ thống 64bit"
+    exit 2
+fi
+
+os_version=""
+
+# os version
+if [[ -f /etc/os-release ]]; then
+    os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
+fi
+if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
+    os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
+fi
+
+if [[ x"${release}" == x"centos" ]]; then
+    if [[ ${os_version} -le 6 ]]; then
+        echo -e "${red}Vui lòng dùng hệ điều hành CentOS 7 trở lên！${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"ubuntu" ]]; then
+    if [[ ${os_version} -lt 16 ]]; then
+        echo -e "${red}Phiên bản Ubuntu 18.04 trở lên！${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"debian" ]]; then
+    if [[ ${os_version} -lt 8 ]]; then
+        echo -e "${red}Phiên bản Debian 8 trở lên！${plain}\n" && exit 1
+    fi
+fi
+
+install_base() {
+    if [[ x"${release}" == x"centos" ]]; then
+        yum install epel-release -y
+        yum install wget curl unzip tar crontabs socat -y
+    else
+        apt update -y
+        apt install wget curl unzip tar cron socat -y
+    fi
+}
+
+# 0: running, 1: not running, 2: not installed
+check_status() {
+    if [[ ! -f /etc/systemd/system/XrayR.service ]]; then
+        return 2
+    fi
+    temp=$(systemctl status XrayR | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+    if [[ x"${temp}" == x"running" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+install_acme() {
+    curl https://get.acme.sh | sh
+}
+
+install_XrayR() {
+    if [[ -e /usr/local/XrayR/ ]]; then
+        rm /usr/local/XrayR/ -rf
+    fi
+
+    mkdir /usr/local/XrayR/ -p
+	cd /usr/local/XrayR/
+
+    if  [ $# == 0 ] ;then
+        last_version=$(curl -Ls "https://api.github.com/repos/overkillzero/xrayr/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        if [[ ! -n "$last_version" ]]; then
+            echo -e "${red}Không xác định được phiên bản XrayR${plain}"
+            exit 1
+        fi
+        echo -e "Phiên bản XrayR mới nhất：v0.9.0，Bắt đầu cài đặt"
+        wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://github.com/overkillzero/xrayr/releases/download/${last_version}/XrayR-linux-${arch}.zip
+        if [[ $? -ne 0 ]]; then
+            echo -e "${red}Không thể tải xuống XrayR, hãy thử lại!${plain}"
+            exit 1
+        fi
+    else
+        if [[ $1 == v* ]]; then
+            last_version=$1
+	else
+	    last_version="v"$1
+	fi
+        url="https://github.com/overkillzero/xrayr/releases/download/${last_version}/XrayR-linux-${arch}.zip"
+        echo -e "Bắt đầu cài đặt XrayR ${last_version}${plain}"
+        wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip ${url}
+        if [[ $? -ne 0 ]]; then
+            echo -e "${red}Phiên bản XrayR v0.9.0 Lỗi, không xác định được phiên bản${plain}"
+            exit 1
+        fi
+    fi
+
+    unzip XrayR-linux.zip
+    rm XrayR-linux.zip -f
+    chmod +x XrayR
+    mkdir /etc/XrayR/ -p
+    rm /etc/systemd/system/XrayR.service -f
+    file="https://raw.githubusercontent.com/overkillzero/xrayr/main/XrayR.service"
+    wget -q -N --no-check-certificate -O /etc/systemd/system/XrayR.service ${file}
+    #cp -f XrayR.service /etc/systemd/system/
+    systemctl daemon-reload
+    systemctl stop XrayR
+    systemctl enable XrayR
+    echo -e "${green}XrayR v0.9.0${plain} Cài đặt hoàn tất!"
+    cp geoip.dat /etc/XrayR/
+    cp geosite.dat /etc/XrayR/ 
+
+    if [[ ! -f /etc/XrayR/config.yml ]]; then
+        cp config.yml /etc/XrayR/
+        echo -e ""
+        echo -e "Để cập nhật phiên bản vui lòng liên hệ admin"
+    else
+        systemctl start XrayR
+        sleep 2
+        check_status
+        echo -e ""
+        if [[ $? == 0 ]]; then
+            echo -e "${green}Khởi động XrayR thành công${plain}"
+        else
+            echo -e "${red}XrayR Không được khởi động được, vui lòng dùng XrayR log để check lỗi${plain}"
+        fi
+    fi
+
+    if [[ ! -f /etc/XrayR/dns.json ]]; then
+        cp dns.json /etc/XrayR/
+    fi
+    if [[ ! -f /etc/XrayR/route.json ]]; then
+        cp route.json /etc/XrayR/
+    fi
+    if [[ ! -f /etc/XrayR/custom_outbound.json ]]; then
+        cp custom_outbound.json /etc/XrayR/
+    fi
+    if [[ ! -f /etc/XrayR/custom_inbound.json ]]; then
+        cp custom_inbound.json /etc/XrayR/
+    fi
+    if [[ ! -f /etc/XrayR/rulelist ]]; then
+        cp rulelist /etc/XrayR/
+    fi
+    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/overkillzero/xrayr/main/xrayz.sh
+    chmod +x /usr/bin/XrayR
+    ln -s /usr/bin/XrayR /usr/bin/xrayr 
+    chmod +x /usr/bin/xrayr
+    cd $cur_dir
+    rm -f install.sh
+    echo -e ""
+    echo "Các lệnh sử dụng XrayR (Không phân biệt in hoa, in thường): "
+    echo "◄▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬►"
+    echo "XrayR                    - Hiện menu"
+    echo "XrayR start              - Khởi chạy XrayR"
+    echo "XrayR stop               - Dừng chạy XrayR"
+    echo "XrayR restart            - Khởi động lại XrayR"
+    echo "XrayR status             - Xem trạng thái XrayR"
+    echo "XrayR enable             - Tự khởi chạy XrayR"
+    echo "XrayR disable            - Hủy tự khởi chạy XrayR"
+    echo "XrayR log                - Xem nhật kí XrayR"
+    echo "XrayR update             - Nâng cấp XrayR"
+    echo "XrayR update x.x.x       - Nâng cấp XrayR đến phiên bản x.x.x"
+    echo "XrayR config             - Hiện thị tệp cấu hình"
+    echo "XrayR install            - Cài đặt XrayR"
+    echo "XrayR uninstall          - Gỡ cài đặt XrayR"
+    echo "XrayR version            - Kiếm tra phiên bản XrayR"
+    echo "◄▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬𝐂𝐎𝐏𝐘𝐑𝐈𝐆𝐇𝐓©𝐇𝐓𝟒𝐆𝐕𝐏𝐍▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬►"
+}
+
+echo -e "${green}BẮT ĐẦU CÀI ĐẶT${plain}"
+install_base
+# install_acme
+install_XrayR $1
